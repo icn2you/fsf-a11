b9c1baadcdf1131f04ca7a54b6e1d473d9acf850
Christopher Zenner <christopher.zenner@gmail.com>
2020-05-15 16:47:31 -0700

    feat: require router for api pages

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require('express'),
       path = require('path');
 
 // Local resources
-const htmlRoutes = require('./app/routes/htmlRoutes');
+const apiRoutes = require('./app/routes/apiRoutes')
+      htmlRoutes = require('./app/routes/htmlRoutes');
 
 // http port
 const PORT = process.env.PORT || 8080;
@@ -15,6 +16,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'app/public')));
+app.use(apiRoutes);
 app.use(htmlRoutes);
 
 app.listen(PORT, () => {
