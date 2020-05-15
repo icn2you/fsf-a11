cd9a3715104803c19417f028a57b73df72a3d906
Christopher Zenner <christopher.zenner@gmail.com>
2020-05-15 15:38:57 -0700

    fix: rearrange order of `app.use()` invocations

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'app/public')));
 app.use(htmlRoutes);
-app.use(morgan('dev'));
 
 app.listen(PORT, () => {
   console.log(`FriendFinder app listening on port ${PORT} ...`);
